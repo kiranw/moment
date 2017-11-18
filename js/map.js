49e0b2acf64075b880f33229d5022b73db50ee0f
Kiran Wattamwar <kiranwattamwar@Kirans-MacBook-Pro.local>
2017-11-18 18:50:23 -0500

    Initializing files

---
+++ js/map.js
@@ -0,0 +1,9 @@
+var api = "AIzaSyAa9Y1ijWZ0zF_vuhvntbJDUgsjrlo4kf0";
+var lat = -2.9450708;
+var lng = -60.6762371;
+
+var request =  "https://maps.googleapis.com/maps/api/streetview?location=" +lat + "," + lng + "&key=" + api;
+
+// console.log(request);
+// $("#maplayout").attr('src', request);
+// console.log("happening");
